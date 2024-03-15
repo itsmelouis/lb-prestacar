7d9d6f4a2ad370665fa0d48d36a5763e58036c30
distrib-ps <theodelhay@orange.fr>
2024-03-15 15:09:26 +0100

    feat: ajout date prestation contact

--- server/api/contact/index.post.ts
+++ server/api/contact/index.post.ts
@@ -8,11 +8,13 @@ interface ContactBody {
   message: string;
   isCondition: boolean;
   isRgpd: boolean;
+  serviceDate: Date;
 }
 
 export default eventHandler<{ body: ContactBody }>(async (event) => {
-  const { firstName, name, email, phone, message, isCondition, isRgpd } = await readBody(event);
+  const { firstName, name, email, phone, message, isCondition, isRgpd, serviceDate } = await readBody(event);
 
+  // Vérification des champs obligatoires
   if (!firstName) {
     throw createError({
       message: "Le prénom est obligatoire.",
@@ -23,13 +25,6 @@ export default eventHandler<{ body: ContactBody }>(async (event) => {
   if (!name) {
     throw createError({
       message: "Le nom est obligatoire.",
-      statusCode: 400,
-    });
-  }
-
-  if (!email) {
-    throw createError({
-      message: "L'email est obligatoire.",
       statusCode: 400,
     });
   }
@@ -48,37 +43,45 @@ export default eventHandler<{ body: ContactBody }>(async (event) => {
     });
   }
 
+  // Vérifications de validité
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   const phoneRegex = /^(0|\+33)[1-9]([-. ]?[0-9]{2}){4}$/;
 
-  if (!emailRegex.test(email)) {
+  if (email && !emailRegex.test(email)) {
     throw createError({
       message: "L'email est invalide.",
       statusCode: 400,
     });
   }
 
-  if(!phoneRegex.test(phone)) {
+  if (phone && !phoneRegex.test(phone)) {
     throw createError({
       message: "Le numéro de téléphone est invalide.",
       statusCode: 400,
     });
   }
 
+  // Vérification que l'email ou le téléphone soit renseigné
+  if (!email && !phone) {
+    throw createError({
+      message: "Au moins un moyen de contact (téléphone ou email) doit être renseigné.",
+      statusCode: 400,
+    });
+  }
 
-  //TODO: gérer le type de prestation puis modifier le schema prisma pour mettre à null l'email et le numéro de 
-  // téléphone puis rajouter une vérif en mode un des deux moyens de contact doit être renseigné
+  // Enregistrement dans la base de données
   try {
     await prisma.contact.create({
       data: {
         prenom: firstName,
         nom: name,
-        email: email,
-        telephone: phone,
-        type_prestation: "contact",
+        email: email ? email : null, // Permet de stocker null si l'email n'est pas renseigné
+        telephone: phone ? phone : null, // Permet de stocker null si le téléphone n'est pas renseigné
+        type_prestation: "contact", // Gestion du type de prestation
         message: message,
         estCondition: isCondition,
         estRgpd: isRgpd,
+        date_presation: serviceDate,
       },
     });
 
