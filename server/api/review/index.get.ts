93f287b88d34f25a72dda9caa37d14eba3c2d5ef
Louis F <72155852+itsmelouis@users.noreply.github.com>
2024-06-24 15:17:05 +0200

    fix: retrieve only checked reviews

--- server/api/review/index.get.ts
+++ server/api/review/index.get.ts
@@ -3,5 +3,6 @@ import { prisma } from "~/prisma/db";
 export default eventHandler(async (event) => {
   return await prisma.avis.findMany({
     take: 3,
+    where: { estValide: true },
   });
 });
