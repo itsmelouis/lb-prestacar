144eb45d7708c83932d348764353eb153eb8bf22
distrib-ps <theodelhay@orange.fr>
2024-06-24 11:28:20 +0200

    feat: add login link footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,5 @@
 <template>
-  <div
-    class="flex flex-col sm:flex-row sm:justify-between sm:items-center bottom-0 py-10 px-4 sm:px-52 space-y-5">
+  <div class="flex flex-col sm:flex-row sm:justify-between sm:items-center bottom-0 py-10 px-4 sm:px-52 space-y-5">
     <div class="mb-6 sm:mb-0 self-center">
       <img src="/logo-white.svg" alt="Logo de PrestaCar" width="150" />
     </div>
@@ -10,17 +9,18 @@
         <NuxtLink to="/conditions-generales" class="transition hover:text-slate-600 duration-300">
           <li>Conditions générales</li>
         </NuxtLink>
+        <NuxtLink to="/login" class="transition hover:text-slate-600 duration-300">
+          <li>Connexion</li>
+        </NuxtLink>
       </ul>
     </div>
     <div class="space-y-2 sm:ml-16">
       <h3>Réseaux sociaux</h3>
       <ul class="flex flex-row sm:flex-col gap-5 sm:gap-0 text-slate-900">
-        <a href="https://www.facebook.com/lbprestacar" target="_blank"
-          class="transition hover:text-slate-600 duration-300">
+        <a href="https://www.facebook.com/lbprestacar" target="_blank" class="transition hover:text-slate-600 duration-300">
           <li>Facebook</li>
         </a>
-        <a href="https://www.instagram.com/lbprestacar/" target="_blank"
-          class="transition hover:text-slate-600 duration-300">
+        <a href="https://www.instagram.com/lbprestacar/" target="_blank" class="transition hover:text-slate-600 duration-300">
           <li>Instagram</li>
         </a>
       </ul>
