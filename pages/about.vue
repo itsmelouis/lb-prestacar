4292d2e69706fbdabe1b717a8e01c87b15a24253
distrib-ps <theodelhay@orange.fr>
2024-06-24 12:13:26 +0200

    feat: add about page

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex flex-col lg:flex-row items-center p-6">
+      <!-- Section Image à gauche -->
+      <div class="lg:w-1/2 mb-6 lg:mb-0 flex justify-center">
+        <div class="aspect-square rounded-xl overflow-hidden">
+          <img alt="Photo" class="object-cover w-full transition-transform hover:scale-105 duration-300" height="400"
+            src="/logo_prestacar.png" style="aspect-ratio: 400/400; object-fit: cover;" width="400" />
+        </div>
+      </div>
+      <!-- Section Texte à droite -->
+      <div class="lg:w-1/2 space-y-4">
+        <p>Ce site est édité par :</p>
+        <p>LB Prestacar dont le siège social est situé 8 rue du maréchal leclerc - 59880 Saint-Saulve.</p>
+        <p>SIRET : 97831201500016</p>
+        <p>Code APE : 4932Z</p>
+        <p>Téléphone : 06 52 74 34 05</p>
+        <p>Adresse de courrier électronique : contact@lb-prestacar.com</p>
+        <p>Directeur de la publication : Lou Baigneaux</p>
+        <p>Responsable du site : Lou Baigneaux</p>
+        <p>Ce site est hébergé par :</p>
+        <p>Hostinger International Ltd, société privée à responsabilité limitée de Chypre, dont l'adresse est 61 Lordou Vironos str. 6023 Larnaca, Chypre</p>
+      </div>
+    </div>
+  </template>
+
+  
