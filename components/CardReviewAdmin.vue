8302f34ce89509ed3d0d66607562bed96077ad4e
distrib-ps <theodelhay@orange.fr>
2024-06-26 11:41:13 +0200

    feat: edit style ; add confirmation before delete

--- components/CardReviewAdmin.vue
+++ components/CardReviewAdmin.vue
@@ -1,7 +1,7 @@
 <template>
   <li class="text-sm leading-6 list-none">
     <div class="relative group flex items-center justify-between">
-      <div class="relative p-6 space-y-6 leading-none rounded-lg bg-white ring-1 ring-yellow-400 flex-grow">
+      <div class="relative p-6 space-y-6 leading-none rounded-lg bg-white ring-1 ring-black flex-grow">
         <div class="flex items-center space-x-4">
           <img
             src="https://t3.ftcdn.net/jpg/05/16/27/58/360_F_516275801_f3Fsp17x6HQK0xQgDQEELoTuERO4SsWV.jpg"
@@ -14,14 +14,35 @@
         <p class="leading-normal text-md">{{ message }}</p>
       </div>
       <div class="flex flex-col space-y-2 ml-4">
-        <button v-if="!isValidated" class="p-2 bg-green-500 text-white rounded-full hover:bg-green-600" @click="handleApprove">
-          <Icon name="check" />
+        <button
+          v-if="!isValidated"
+          class="flex items-center justify-center w-12 h-12 border-2 border-green-500 rounded-full text-green-500 hover:text-green-700 hover:border-green-700"
+          @click="handleApprove"
+        >
+          <Icon name="material-symbols:check" class="text-3xl"/>
         </button>
-        <button class="p-2 bg-red-500 text-white rounded-full hover:bg-red-600" @click="handleDelete">
-          <Icon name="times" />
+        <button
+          class="flex items-center justify-center w-12 h-12 border-2 border-red-500 rounded-full text-red-500 hover:text-red-700 hover:border-red-700"
+          @click="confirmDelete"
+        >
+          <Icon name="radix-icons:cross-1" class="text-3xl"/>
         </button>
       </div>
     </div>
+    <Dialog v-model:visible="showDialog" :modal="true" :dismissable-mask="false" :style="{ width: '400px' }">
+      <template #header>
+        <h3>Confirmation</h3>
+      </template>
+      <div class="p-4">
+        <p>Voulez-vous vraiment supprimer cet avis ?</p>
+      </div>
+      <template #footer>
+        <div class="flex justify-end space-x-2">
+          <Button label="Annuler" @click="showDialog = false" />
+          <Button label="Supprimer" severity="primary" @click="handleDelete" />
+        </div>
+      </template>
+    </Dialog>
   </li>
 </template>
 
@@ -38,6 +59,7 @@ interface CardReviewProps {
 
 const props = defineProps<CardReviewProps>();
 const emit = defineEmits(['approve', 'delete']);
+const showDialog = ref(false);
 
 const handleApprove = () => {
   emit('approve', props.id);
@@ -45,6 +67,11 @@ const handleApprove = () => {
 
 const handleDelete = () => {
   emit('delete', props.id);
+  showDialog.value = false;
+};
+
+const confirmDelete = () => {
+  showDialog.value = true;
 };
 </script>
 
